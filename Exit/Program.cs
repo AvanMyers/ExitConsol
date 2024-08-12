785c9c418d7a4568e9ff0c5dcdb969dab2e42e6e
AvanMyers <smail1341@gmail.com>
2024-08-12 15:43:18 +0300

    Initial commit

---
+++ Exit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isEnd = false;
+            string exit = "exit";
+            string message;
+
+            Console.WriteLine($"Сообщение будет выводится пока вы не напишите {exit}");
+
+            while (isEnd == false) {
+                Console.WriteLine("Введите сообщение");
+                message = Console.ReadLine();
+                if (message == exit)
+                {
+                    Console.WriteLine("Конец");
+                    isEnd = true;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
